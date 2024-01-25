c27d81cc9a4d3a597b5a231a5d819a6d0a340b82
Maria Kovalevskaya <35896507+maryartkey@users.noreply.github.com>
2024-01-25 16:04:32 +0700

    Update lab2.cu

--- lab2/lab2.cu
+++ lab2/lab2.cu
@@ -1,6 +1,7 @@
 #include <cuda_runtime.h>
 #include <iostream>
 #include <png.h>
+#include <string>
 #define CHANNELS 3
 
 const char *filename_in = "cat.png";
@@ -22,17 +23,14 @@ void readPNG(const char *filename, int &height, int &width)
     
     height = png_get_image_height(png_ptr, info_ptr);
     width = png_get_image_width(png_ptr, info_ptr);	
-    
     *row_pointers = (png_bytep *) malloc(sizeof(png_bytep) * height);
         for (int i = 0; i < height; ++i) 
         {
             (*row_pointers)[i] = (png_byte *) malloc(sizeof(png_byte) * width * CHANNELS);
         }
-
     png_read_image(png_ptr, (*row_pointers));
     png_destroy_read_struct(&png_ptr, NULL, NULL); 
     fclose(fp);
-
 }
 
 void writePNG(const char *filename)
@@ -47,7 +45,6 @@ void writePNG(const char *filename)
     png_write_image(png_ptr, (*row_pointers));
     png_destroy_write_struct(&png_ptr, &info_ptr);
     fclose(fp);
-
 }
 
 __global__ void blurKernelGlobal(unsigned char* in, unsigned char* out, int width, int height) 
@@ -57,7 +54,7 @@ __global__ void blurKernelGlobal(unsigned char* in, unsigned char* out, int widt
 
     if (col < width && row < height) 
     {
-        int blurSize = 1; // You can adjust the blur size
+        int blurSize = 5; // You can adjust the blur size
         int pixVal = 0;
         int pixels = 0;
 
@@ -81,39 +78,138 @@ __global__ void blurKernelGlobal(unsigned char* in, unsigned char* out, int widt
     }
 }
 
+__global__ void blurKernelShared(unsigned char* in, unsigned char* out, int width, int height) 
+{
+    int blurSize = 5; // You can adjust the blur size
+    int col = blockIdx.x * blockDim.x + threadIdx.x - (2 * blockIdx.x + 1) * blurSize;
+    int row = blockIdx.y * blockDim.y + threadIdx.y - (2 * blockIdx.y + 1) * blurSize;
+
+    int idx = (row * width + col) * CHANNELS;
+    if (col < width && row < height) 
+    {        
+        int pixVal = 0;
+        int pixels = 0;
+
+        // Average the surrounding pixels
+        for (int blurRow = -blurSize; blurRow <= blurSize; blurRow++) 
+        {
+            for (int blurCol = -blurSize; blurCol <= blurSize; blurCol++) 
+            {
+                int curRow = row + blurRow;
+                int curCol = col + blurCol;
+
+                // Check for boundary conditions
+                if (curRow > -1 && curRow < height && curCol > -1 && curCol < width) 
+                {
+                    pixVal += in[curRow * width + curCol];
+                    pixels++;
+                }
+            }
+        }
+        out[row * width + col] = (unsigned char)(pixVal / pixels);
+    }
+}
+
+__global__ void blurKernelTexture(unsigned char* out, int width, int height) 
+{
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (col < width && row < height) 
+    {
+        int blurSize = 5; // You can adjust the blur size
+        int pixVal = 0;
+        int pixels = 0;
+
+        // Average the surrounding pixels
+        for (int blurRow = -blurSize; blurRow <= blurSize; blurRow++) 
+        {
+            for (int blurCol = -blurSize; blurCol <= blurSize; blurCol++) 
+            {
+                int curRow = row + blurRow;
+                int curCol = col + blurCol;
+
+                // Check for boundary conditions
+                if (curRow > -1 && curRow < height && curCol > -1 && curCol < width) 
+                {
+                    pixVal += in[curRow * width + curCol];
+                    pixels++;
+                }
+            }
+        }
+        out[row * width + col] = (unsigned char)(pixVal / pixels);
+    }
+}
+
+void applyBlur(const unsigned char *inputImage, unsigned char *outputImage, size_t imageSize, int width, int height, const char* memoryType) 
+{    
+    unsigned char *d_inputImage, *d_outputImage;
+    cudaMalloc((void ) &d_inputImage, imageSize);
+    cudaMalloc((void **) &d_outputImage, imageSize);
+    cudaMemcpy(d_inputImage, inputImage, imageSize, cudaMemcpyHostToDevice);
+
+    int blockSize = 32;
+    int blurSize = 1;
+    
+    dim3 GridSize(width / blockSize, height / blockSize, 1);
+    dim3 BlockSize(blockSize, blockSize, 1);
+    
+    auto start_time = std::chrono::high_resolution_clock::now();
+    if (type == "global") 
+    {
+        blurKernelGlobal<<<GridSize,BlockSize>>>(d_inputImage, d_outputImage, width, height);
+        auto end_time = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+        std::cout << "Time:" << duration << "us." << std::endl;
+    } 
+    else if (type == "shared") 
+    {
+        blurKernelShared<<<GridSize,BlockSize>>>(d_inputImage, d_outputImage, width, height);
+        auto end_time = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+        std::cout << "Time:" << duration << "us." << std::endl;
+    } 
+    else if (type == "texture") 
+    {
+        blurKernelTexture<<<GridSize, BlockSize>>>(d_outputImage, width, height);
+        auto end_time = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+        std::cout << "Time:" << duration << "us." << std::endl;
+    }   
+    
+    cudaDeviceSynchronize();
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+
+    std::cout << "Time:" << duration << "us." << std::endl;
+
+    cudaMemcpy(outputImage, d_outputImage, imageSize, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_inputImage);
+    cudaFree(d_outputImage);
+}
+
 int main()
 {    
     int height;
     int width;
     read_png(filename_in, height, width);
 
-    unsigned char* h_inImage = new unsigned char[width * height];
-    unsigned char* h_outImage = new unsigned char[width * height];
+   size_t imageSize = width * height * CHANNELS * sizeof(unsigned char);
+    auto *inputImage = new unsigned char[imageSize];
+    auto *outputImage = new unsigned char[imageSize]; 
 
-    // Allocate memory on the device
-    unsigned char* d_inImage;
-    unsigned char* d_outImage;
-    cudaMalloc(&d_inImage, width * height * sizeof(unsigned char));
-    cudaMalloc(&d_outImage, width * height * sizeof(unsigned char));
     auto start_time = std::chrono::high_resolution_clock::now();
     // Copy data from host to device
     cudaMemcpy(d_inImage, h_inImage, width * height * sizeof(unsigned char), cudaMemcpyHostToDevice);
 
     // Kernel launch parameters
-    dim3 dimBlock(16, 16);
-    dim3 dimGrid((width + dimBlock.x - 1) / dimBlock.x, (height + dimBlock.y - 1) / dimBlock.y);    
-
-    // Launch the kernel
-    blurKernel<<<dimGrid, dimBlock>>>(d_inImage, d_outImage, width, height);
-
-    // Copy result back to host
-    cudaMemcpy(h_outImage, d_outImage, width * height * sizeof(unsigned char), cudaMemcpyDeviceToHost);
-    
+    applyBlur(inputImage, outputImage, imageSize, width, height, "global");
+    applyBlur(inputImage, outputImage, imageSize, width, height, "shared");
+    applyBlur(inputImage, outputImage, imageSize, width, height, "texture");
+        
     write_png(filename_out);
-    
-    auto end_time = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
-    std::cout << "Время:" << duration << "мкс." << std::endl;
 
     // Free device memory
     cudaFree(d_inImage);
